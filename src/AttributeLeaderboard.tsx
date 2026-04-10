be5ab1fcc53af1be71cf3aaa5542319c5c9bd4ed
Ano <anodozes@gmail.com>
2026-04-09 23:20:38 -0400

    begin building attribute leaderboards

---
+++ src/AttributeLeaderboard.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { groupByBoard, fetchAttributeLeaderboard } from "./db"
+import type { AttributeLeaderboardRow } from "./db"
+import { useParams } from "react-router"
+import Leaderboard from "./Leaderboard"
+import './leaderboard.css'
+
+const ATTRIBUTE_ORDER = ['BA', 'BABIP', 'HR']
+
+function AttributeLeaderboards(){
+  // const { leagueID } = useParams() todo: change routing and use params so peopel can bookmark their league
+  const [sickoData, setSickoData] = useState<Record<string, AttributeLeaderboardRow[]> | null>(null)
+
+    useEffect(() => {
+    setSickoData(null)
+    console.log("fetching leaderboards...")
+
+    document.title = "sickosyes.png"
+    
+    const sickoFetch = fetchAttributeLeaderboard();
+
+    sickoFetch.then(data =>{
+      const boards = groupByBoard(data)
+      setSickoData(boards)
+    })
+  }, [])
+
+  const sickoLeaders = battingData ? Object.entries(battingData).map(([statKey, leaderboard]) => {
+    return (
+      <Leaderboard key={statKey} 
+      leaderboardType={'attribute'}
+      statKey={statKey} 
+      leaderboard={leaderboard}  
+      />
+    )})
+    : <p>loading...</p>
+
+ return (
+    <div>
+      <div className="leaderboard-container">
+        {sickoLeaders}
+      </div>
+    </div>
+  )
+}
+
+export default AttributeLeaderboards;
