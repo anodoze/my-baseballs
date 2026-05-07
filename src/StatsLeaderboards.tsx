dd75b9609bb0d0394f87133e74c3aa8ff6eec7e2
Ano <anodozes@gmail.com>
2026-05-06 22:53:32 -0400

    fix walks not displaying on LL boards

--- src/StatsLeaderboards.tsx
+++ src/StatsLeaderboards.tsx
@@ -19,8 +19,8 @@ const BATTING_STAT_ORDER = [
   'On Base Plus Slugging (OPS)', 'Batting Average on Balls in Play (BABIP)',
   'Hits', 'Singles', 'Doubles', 'Triples', 'Home Runs',
   'Runs', 'Runs Batted In (RBI)',
-  'Walks', 'Hit By Pitch (HBP)', 'Stolen Bases',
-  'Caught Stealing', 'Struck Out' 
+  'Walked', 'Hit By Pitch (HBP)', 'Stolen Bases',
+  'Caught Stealing', 'Struck Out'
 ]
 
 const PITCHING_STAT_ORDER = [
